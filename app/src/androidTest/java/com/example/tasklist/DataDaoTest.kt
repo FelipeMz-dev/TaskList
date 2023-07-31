96e16d571cf04aadfb2a7d5bed86160a25fc4881
FelipeMz <felipemendez.ps2.dev@gmail.com>
2023-07-30 21:19:30 -0500

    Pruebas unitarias

---
+++ app/src/androidTest/java/com/example/tasklist/DataDaoTest.kt
@@ -0,0 +1,54 @@
+package com.example.tasklist
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.tasklist.Data.DataBase.Dao.TaskDao
+import com.example.tasklist.Data.DataBase.TaskDataBase
+import com.example.tasklist.Data.Entities.TaskEntity
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DataDaoTest {
+    @Rule
+    @JvmField
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: TaskDataBase
+    private lateinit var dataDao: TaskDao
+
+    @Before
+    fun setup() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        database = Room.inMemoryDatabaseBuilder(context, TaskDataBase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        dataDao = database.taskDao()
+    }
+
+    @After
+    fun closeDb() {
+        database.close()
+    }
+
+    @Test
+    fun insertDataTest() = runBlocking {
+        val data = TaskEntity(id = 1,
+            taskText = "do the homework",
+            expiryDate = "31/07/2023",
+            listSteps = "complete the first step",
+            done = false
+        )
+        dataDao.insertTask(data)
+
+        val insertedData = dataDao.getTaskById(1)
+        assert(insertedData != null)
+        assert(insertedData?.taskText == "do the homework")
+    }
+}
